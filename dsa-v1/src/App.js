9a9c4e91598773a2d3a51e278f5d7b04977b0ae8
ABHISHEK DHARWAL <56113773+elite-abhishek-27@users.noreply.github.com>
2021-07-27 00:27:23 +0530

    axios update

--- dsa-v1/src/App.js
+++ dsa-v1/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import Whole from "./components/Whole/Whole";
+import api from "./config/axios";
 
 function App() {
+  api
+    .get("/user/user/60fc470820f0124cb79793d0")
+    .then((res) => console.log(res));
   return (
     <div className="App">
       <Whole />
